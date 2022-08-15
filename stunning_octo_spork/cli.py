7a3f08d73daf89a21e440b836148614e46b9f6da
Shota Nakagawa <ns6251@gmail.com>
2022-08-16 03:17:04 +0900

    Add two cmd args

--- stunning_octo_spork/cli.py
+++ stunning_octo_spork/cli.py
@@ -12,6 +12,8 @@ from .logentry import LogEntry
 class Args:
     log: Path
     n: int
+    m: int
+    t: int
 
     @staticmethod
     def parse_args(argv: Sequence[str] | None = None) -> "Args":
@@ -23,10 +25,14 @@ class Args:
         """
         parser = argparse.ArgumentParser(description="監視ログの統計")
         parser.add_argument("path", nargs="?", type=Path, help="読み込む監視ログのパス")
-        parser.add_argument("-n", nargs="?", default=1, type=int, help="故障判定に必要な連続タイムアウトの回数", metavar="N")
+        parser.add_argument("-n", nargs="?", default=1, type=int, help="故障判定に必要な連続タイムアウトの回数（デフォルト: 1）", metavar="N")
+        parser.add_argument("-m", nargs="?", default=1, type=int, help="過負荷判定に用いる直近の回数（デフォルト: 1）", metavar="M")
+        parser.add_argument(
+            "-t", nargs="?", default=500, type=int, help="過負荷判定の平均応答時間の閾値（デフォルト: 500 [ms]）", metavar="T"
+        )
         args = parser.parse_args(argv)
         log = cast(Path, args.path)
-        return Args(log, cast(int, args.n))
+        return Args(log, args.n, args.m, args.t)
 
 
 def main() -> None:
